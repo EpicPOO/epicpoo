10c3a7b807a3b4bf043f5b01418f78ec9be8d3fe
BuharuVlad <75388031+BuharuVlad@users.noreply.github.com>
2021-01-16 22:55:48 +0200

    Update meniu.h
    Meniul este complex functional

--- meniu.h
+++ meniu.h
@@ -11,6 +11,33 @@
 
 using namespace std;
 
+string optiuniUtilizatore = "1 - Film\n2 - Sala\n3 - Bilete\n4 - Aliment\n5 - Angajat\n0 - Revino la meniul anterior\n";
+int nrOptiuniUtilizator = 5; //nu include optiunea de returnare la meniul anterior (0)
+
+int ruleazaMeniuUtilizator(string optiuniUtilizator, int nrOptiuniUtilizator)
+{
+	int optiune = 0;
+	cout << endl << optiuniUtilizatore;
+	cin >> optiune;
+	while (optiune > nrOptiuni || optiune < 0)
+	{
+		cout << "Te rog introdu o optiune valida!" << endl;
+		cout << optiuniUtilizatore;
+		cin >> optiune;
+	}
+	return optiune;
+}
+int meniuAnteriorUtilizator()
+{
+	cout << endl << "Vrei sa revii la meniul anterior?" << endl << endl << "1 - Da" << endl << "0 - Nu" << endl;
+	int tmp = 0;
+	do
+	{
+		cin >> tmp;
+		if (tmp == 0 || tmp == 1) return tmp;
+	} while (tmp != 0 && tmp != 1);
+}
+
 void meniu()
 {
 	if (testareExistentaFisiere() == 0)
@@ -19,6 +46,71 @@ void meniu()
 	}
 	else
 	{
+
+		int optiune;
+		string buffer;
+		
+		int alegereCategorie = 0;
+		int alegereSubmeniu1 = 0;
+		do
+		{
+			cout << "Alegeti!" << endl << "1. Client" << endl;
+			cout << "2. Utilizator" << endl;
+			cin >> alegereCategorie;
+			switch (alegereCategorie)
+			{
+			case 1:
+				break;
+			case 2:
+			{
+				do
+				{
+					optiune = ruleazaMeniuUtilizator(optiuniFilme, nrOptiuniFilme);
+					{
+						switch (optiune) {
+						case 0:
+							break;
+						case 1: //film
+						{
+							meniuFilm();
+							if (meniuAnteriorFilm() != 1) optiune = 0;
+						}
+						break;
+						case 2://sala
+						{
+							meniuSala();
+							if (meniuAnteriorFilm() != 1) optiune = 0;
+						}
+						break;
+
+						case 3://bilet
+						{
+							meniuBilet();
+							if (meniuAnteriorFilm() != 1) optiune = 0;
+						}
+						break;
+						case 4: //aliment
+						{
+							meniuAliment();
+							if (meniuAnteriorFilm() != 1) optiune = 0;
+						}
+						break;
+						case 5://angajat
+						{
+							meniuAngajat();
+							if (meniuAnteriorFilm() != 1) optiune = 0;
+						}
+						break;
+					deafult:
+						break;
+						}
+					}
+				} while (optiune != 0);
+			}
+			}
+		} while (alegereCategorie != 1 || alegereCategorie != 2);
+
+		/*
 		cout << endl << "1. Client" << endl;
 		cout << "2. Utilizator" << endl;
 		int alegereCategorie = 0;
@@ -43,16 +135,13 @@ void meniu()
 				{
 					switch (alegereSubmeniu1)
 					{
-					case 1:
-						//film
+					case 1:	//film
 						meniuFilm();
 						break;
-					case 2:
+					case 2: //sala
 						meniuSala();
-						//sala
 						break;
-					case 3:
-						//bilete
+					case 3://bilete
 						meniuBilet();
 						break;
 					case 4:
@@ -80,7 +169,9 @@ void meniu()
 				cin >> alegereCategorie;
 				break;
 			}
-		} while (alegereCategorie != 1 || alegereCategorie != 2);
+		}
+	}
+	*/
 	}
 }
 
